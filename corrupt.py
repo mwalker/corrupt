847c5d566f88163727d1833fff403c2e14db50e6
Matthew Walker <matthew@gunzel.org>
2025-10-31 12:00:04 +1100

    Updates for python 3

--- corrupt.py
+++ corrupt.py
@@ -5,16 +5,16 @@ import argparse
 import string
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input', nargs='?', type=argparse.FileType('r'), 
-        default=sys.stdin,
+parser.add_argument('input', nargs='?', type=argparse.FileType('rb'),
+        default=sys.stdin.buffer,
         help='input file. defaults to stdin')
-parser.add_argument('-o', '--output', type=argparse.FileType('w'), 
-        default=sys.stdout, action='store',
+parser.add_argument('-o', '--output', type=argparse.FileType('wb'),
+        default=sys.stdout.buffer, action='store',
         help='output filename. defaults to stdout')
 parser.add_argument('-n', action='store', type=int, default=1000000,
-        help='''average good bits per error. 
+        help='''average good bits per error.
                 defaults to 1000000 (10e-6 bit error rate)''')
-parser.add_argument('-t', '--truncate', action='store', 
+parser.add_argument('-t', '--truncate', action='store',
         type=int, default=None,
         help='truncate file after T bytes')
 parser.add_argument('-g', '--garbage', action='store', type=int, default=0,
@@ -30,13 +30,14 @@ nextn = random.randint(0,args.n)
 data = args.input.read(CHUNK)
 while len(data) > 0:
     cend = cstart + len(data)
-    while cend > nextn/8:
+    while cend > nextn//8:
         k = nextn - cstart * 8
         if args.ascii:
-            b = random.choice(string.printable)
+            b = random.choice(string.printable).encode('ascii')
         else:
-            b = chr(ord(data[k/8]) ^ (1 << (k % 8)))
-        data = ''.join([data[:k/8], b, data[k/8+1:]])
+            byte_idx = k//8
+            b = bytes([data[byte_idx] ^ (1 << (k % 8))])
+        data = data[:byte_idx] + b + data[byte_idx+1:]
         nextn += random.randint(0,2*args.n)
     cstart = cend
     if args.truncate is not None and cend > args.truncate:
@@ -49,9 +50,9 @@ random.seed()
 garbage = args.garbage
 while garbage > 0:
     if args.ascii:
-        b = random.choice(string.printable)
+        b = random.choice(string.printable).encode('ascii')
     else:
-        b = chr(random.randint(0, 255))
+        b = bytes([random.randint(0, 255)])
     args.output.write(b)
     garbage -= 1
 
